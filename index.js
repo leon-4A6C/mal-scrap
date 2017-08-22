f2ad2efd959fedcb91f2eda178f7e2ef7437e470
leon in 't Veld <leon3110l@gmail.com>
2017-08-22 21:43:47 +0200

    split the functions up in one function

--- index.js
+++ index.js
@@ -43,6 +43,51 @@ class MAL {
       .then(res => res.text())
   }
 
+  _parseTopTr(tr, type="anime") {
+    const out = {
+      id: parseInt(tr.find(".detail .hoverinfo_trigger").attr("id").replace("#area", "")),
+      ranking: parseInt(tr.find(".top-anime-rank-text").text()),
+      title: tr.find(".detail .hoverinfo_trigger").text().replace(/\s\s+/g, ' ').trim(),
+      href: tr.find(".hoverinfo_trigger").attr("href"),
+      score: parseFloat(tr.find(".score .text").text()),
+      posters: tr.find("img").data()
+    };
+
+    // get small posters
+    const posters = {};
+    const x = out.posters.srcset.split(", ");
+    for (let src in x) {
+      const piece = x[src].split(" ");
+      posters[piece[1]] = piece[0];
+    }
+    out.posters.srcset = posters;
+
+    // get big poster
+    const posterBase = `https://myanimelist.cdn-dena.com/images/${type}`
+    const pos = (out.posters.src.indexOf(`${type}/`)+type.length);
+    const posterId = out.posters.src.substring(pos, out.posters.src.indexOf(".", pos)+4);
+    out.posters.id = posterId;
+    out.posters.big = posterBase + posterId;
+
+    // parse information
+    let info = tr.find(".information").text()
+      .trim()
+      .split("\n")
+      .map(x => x.replace(/\s\s+/g, " "))
+
+    info = {
+      runtime: info[1].trim(),
+      members: parseInt(info[2].replace(" members", "").replace(",", "").trim()),
+      episodes: parseInt(info[0].replace(/[a-zA-Z()?]/g, "").trim()) || 0,
+      type: info[0].replace(/[0-9()?]/g, "").replace(type == "anime" ? "eps" : "vols", "").trim()
+    }
+    out.type = info.type;
+    // add the info to the out
+    out.info = info;
+
+    return out;
+  }
+
   topAnime(options) {
     return this._get(endpoints.topAnime, options)
       .then(text => {
@@ -51,52 +96,12 @@ class MAL {
             const output = [];
 
             const $ = cheerio.load(text);
-            $(".top-ranking-table tbody").children().each(function(i, elem) {
-              const tr = $(this);
+            $(".top-ranking-table tbody").children().each((i, elem) => {
+              const tr = $(elem);
               if (tr.hasClass("ranking-list")) {
-                const out = {
-                  id: parseInt(tr.find(".detail .hoverinfo_trigger").attr("id").replace("#area", "")),
-                  ranking: parseInt(tr.find(".top-anime-rank-text").text()),
-                  title: tr.find(".detail .hoverinfo_trigger").text().replace(/\s\s+/g, ' ').trim(),
-                  href: tr.find(".hoverinfo_trigger").attr("href"),
-                  score: parseFloat(tr.find(".score .text").text()),
-                  posters: tr.find("img").data()
-                };
-
-                // get small posters
-                const posters = {};
-                const x = out.posters.srcset.split(", ");
-                for (let src in x) {
-                  const piece = x[src].split(" ");
-                  posters[piece[1]] = piece[0];
-                }
-                out.posters.srcset = posters;
-
-                // get big poster
-                const posterBase = "https://myanimelist.cdn-dena.com/images/anime"
-                const pos = (out.posters.src.indexOf("anime/")+5);
-                const posterId = out.posters.src.substring(pos, out.posters.src.indexOf(".", pos)+4);
-                out.posters.id = posterId;
-                out.posters.big = posterBase + posterId;
-
-                // parse information
-                let info = tr.find(".information").text()
-                  .trim()
-                  .split("\n")
-                  .map(x => x.replace(/\s\s+/g, " "))
-
-                info = {
-                  runtime: info[1].trim(),
-                  members: parseInt(info[2].replace(" members", "").replace(",", "").trim()),
-                  episodes: parseInt(info[0].replace(/[a-zA-Z()]/g, "").trim()),
-                  type: info[0].replace(/[0-9()]/g, "").replace("eps", "").trim()
-                }
-                out.type = info.type;
-                // add the info to the out
-                out.info = info;
 
                 // push it to the output
-                output.push(out);
+                output.push(this._parseTopTr(tr));
               }
             });
             resolve(output);
@@ -115,52 +120,12 @@ class MAL {
             const output = [];
 
             const $ = cheerio.load(text);
-            $(".top-ranking-table tbody").children().each(function(i, elem) {
-              const tr = $(this);
+            $(".top-ranking-table tbody").children().each((i, elem) => {
+              const tr = $(elem);
               if (tr.hasClass("ranking-list")) {
-                const out = {
-                  id: parseInt(tr.find(".detail .hoverinfo_trigger").attr("id").replace("#area", "")),
-                  ranking: parseInt(tr.find(".top-anime-rank-text").text()),
-                  title: tr.find(".detail .hoverinfo_trigger").text().replace(/\s\s+/g, ' ').trim(),
-                  href: tr.find(".hoverinfo_trigger").attr("href"),
-                  score: parseFloat(tr.find(".score .text").text()),
-                  posters: tr.find("img").data()
-                };
-
-                // get small posters
-                const posters = {};
-                const x = out.posters.srcset.split(", ");
-                for (let src in x) {
-                  const piece = x[src].split(" ");
-                  posters[piece[1]] = piece[0];
-                }
-                out.posters.srcset = posters;
-
-                // get big poster
-                const posterBase = "https://myanimelist.cdn-dena.com/images/manga"
-                const pos = (out.posters.src.indexOf("manga/")+5);
-                const posterId = out.posters.src.substring(pos, out.posters.src.indexOf(".", pos)+4);
-                out.posters.id = posterId;
-                out.posters.big = posterBase + posterId;
-
-                // parse information
-                let info = tr.find(".information").text()
-                  .trim()
-                  .split("\n")
-                  .map(x => x.replace(/\s\s+/g, " "))
-
-                info = {
-                  runtime: info[1].trim(),
-                  members: parseInt(info[2].replace(" members", "").replace(",", "").trim()),
-                  volumes: parseInt(info[0].replace(/[a-zA-Z()?]/g, "").trim()) || 0,
-                  type: info[0].replace(/[0-9()?]/g, "").replace("vols", "").trim()
-                }
-                out.type = info.type;
-                // add the info to the out
-                out.info = info;
 
                 // push it to the output
-                output.push(out);
+                output.push(this._parseTopTr(tr, "manga"));
               }
             });
             resolve(output);
@@ -173,5 +138,5 @@ class MAL {
 }
 
 const client = new MAL();
-client.topManga()
+client.topAnime()
   .then(output => console.log(output))
